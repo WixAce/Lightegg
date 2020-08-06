3b626ff64a2de028ab6201c0d18180e93dfc2a2c
wiix <1136833646@qq.com>
2020-08-07 03:45:02 +0800

    rename scripts

---
+++ Assets/Scripts/Lightegg/Editor/LightEggEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class LightEggEditor : Editor
+{
+    
+    [MenuItem("Appegg/Clear Cache",false,1000)]
+    static void ClearCache() =>Caching.ClearCache();
+    
+}
